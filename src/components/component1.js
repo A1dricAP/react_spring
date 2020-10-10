6ff980c898171c54590ae66cafe18ce1481dcde9
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-10-10 12:26:33 +0530

    Simple Animations In React-using React Spring

---
+++ src/components/component1.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Spring} from 'react-spring/renderprops'
+export default function component1() {
+    return (
+        <Spring from={{opacity:0 , marginTop:-500}} to={{opacity:1, marginTop:0}}>
+            {props=>(
+                <div style={props}>
+                    <div style={c1style}>
+            <h1>Component 1</h1>
+            <p>
+            publishing and graphic design, Lorem ipsum is a placeholder 
+            text commonly used to demonstrate the visual form of a document 
+            or a typeface without relying on meaningful content. 
+            Lorem ipsum may be used before final copy is available, but 
+            it may also be used to temporarily replace copy in a process called greeking, 
+            which allows designers to consider form without the meaning of the text influencing 
+            the design.
+            </p>
+        </div>
+                </div>
+            )}
+        </Spring>
+        
+    )
+}
+
+const c1style={
+    background:'steelblue',
+    color:'white',
+    padding:'1.5rem'
+}
---
+++ src/components/component2.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import {Spring} from 'react-spring/renderprops'
+
+export class component2 extends Component {
+    render() {
+        return (
+            <Spring from={{opacity:0 }} to={{opacity:1}} config={{delay:1000, duration:1000}}>
+            {props=>(
+                <div style={props}>
+                    <div style={c2style}>
+            <h1>Component 1</h1>
+            <p>
+            publishing and graphic design, Lorem ipsum is a placeholder 
+            text commonly used to demonstrate the visual form of a document 
+            or a typeface without relying on meaningful content. 
+            Lorem ipsum may be used before final copy is available, but 
+            it may also be used to temporarily replace copy in a process called greeking, 
+            which allows designers to consider form without the meaning of the text influencing 
+            the design.
+            </p>
+        </div>
+                </div>
+            )}
+        </Spring>
+        )
+    }
+}
+
+const c2style={
+    background:'slateblue',
+    color:'white',
+    padding:'1.5rem'
+}
+
+export default component2
